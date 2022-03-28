c7a769cedbcd1cd849eb5d95debf3ec527ef1eb0
Stephan Sinn <stephan.sinn@d-fine.de>
2022-03-28 13:09:56 +0200

    ktlintformat

--- api/src/main/kotlin/org/eclipse/dataspaceconnector/extensions/api/ApiEndpointExtension.kt
+++ api/src/main/kotlin/org/eclipse/dataspaceconnector/extensions/api/ApiEndpointExtension.kt
@@ -25,7 +25,6 @@ import org.eclipse.dataspaceconnector.spi.WebService
 import org.eclipse.dataspaceconnector.spi.system.ServiceExtension
 import org.eclipse.dataspaceconnector.spi.system.ServiceExtensionContext
 
-
 @OpenAPIDefinition(info = Info(title = "Dataland EDC OpenAPI Spec", version = "1.0.0-SNAPSHOT"))
 
 class ApiEndpointExtension : ServiceExtension {
