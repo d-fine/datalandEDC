8cf422269c69e909a8c55586f2fb182056ab768c
Emanuel Alexandi <emanuel.alexandi@d-fine.de>
2022-04-25 18:02:50 +0200

    Fix?

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -47,13 +47,12 @@ tasks.test {
     }
 }
 val rsApi: String by project
-
 dependencies {
-    implementation("org.eclipse.dataspaceconnector:test") //klappt nicht
-    implementation("org.eclipse.dataspaceconnector:util") //klappt nicht
+    //implementation("org.eclipse.dataspaceconnector:test") //klappt nicht
+    //implementation("org.eclipse.dataspaceconnector:util") //klappt nicht
     implementation("org.eclipse.dataspaceconnector:spi") //klappt
     implementation("io.swagger.core.v3:swagger-annotations:2.1.13")
-    implementation("org.eurodat.broker:controller") //klappt nicht
+    implementation("org.eurodat:broker-extension") //klappt nicht
     //implementation(project(":trustee-platform:services:broker"))
     api("jakarta.ws.rs:jakarta.ws.rs-api:$rsApi")
 
--- api/src/main/kotlin/org/eclipse/dataspaceconnector/extensions/controller/CustomAssetController.kt
+++ api/src/main/kotlin/org/eclipse/dataspaceconnector/extensions/controller/CustomAssetController.kt
@@ -1,13 +1,16 @@
 package org.eclipse.dataspaceconnector.extensions.controller
 
+import org.eclipse.dataspaceconnector.policy.model.Action
+import org.eclipse.dataspaceconnector.policy.model.Permission
+import org.eclipse.dataspaceconnector.policy.model.Policy
 import org.eclipse.dataspaceconnector.spi.contract.negotiation.ConsumerContractNegotiationManager
 import org.eclipse.dataspaceconnector.spi.contract.negotiation.store.ContractNegotiationStore
 import org.eclipse.dataspaceconnector.spi.contract.offer.store.ContractDefinitionStore
 import org.eclipse.dataspaceconnector.spi.monitor.ConsoleMonitor
-import org.eclipse.dataspaceconnector.util.*
-import org.eclipse.dataspaceconnector.test.*
+import org.eclipse.dataspaceconnector.spi.types.domain.asset.Asset
 import org.eurodat.broker.controller.AssetController
 import java.io.File
+import java.net.URI
 
 class CustomAssetController {
 
@@ -40,7 +43,10 @@ class CustomAssetController {
         return endpoint
     }
 
-    //Asset Controller benutzen
+
+
+
+    //____________________________________________Asset Controller benutzen
     fun registerAsset(data: String): String{
         //val endpoint = buildDynamicEndpoint(buildJson(data))
         val providerRequest: ProviderRequest = ProviderRequest() //bauen
@@ -51,5 +57,58 @@ class CustomAssetController {
         bei EuroDaT liegen*/
 
         return dataId
+    }
+
+
+
+    //___________________________________________ Auf Basis von E2E-Test:
+
+    private val jsonMapper = jacksonObjectMapper()
+
+    private val testCredentials = "password"
+    private val trusteeClient = HttpClient(
+        DefaultOkHttpClientFactoryImpl.create(false), trusteeURL, "APIKey", testCredentials)
+    private val consumerClient = HttpClient(
+        DefaultOkHttpClientFactoryImpl.create(false), consumerURL, "APIKey", testCredentials)
+
+    private val action = Action.Builder.newInstance()
+        .type("USE")
+        .build()
+    private val permission = Permission.Builder.newInstance()
+        .target("test-asset")
+        .action(action)
+        .build()
+    private val asset = Asset.Builder.newInstance()
+        .id("test-asset")
+        .property("endpoint", "https://filesamples.com/samples/code/json/sample2.json")
+        .build()
+    private val policy = Policy.Builder.newInstance()
+        .id("956e172f-2de1-4501-8881-057a57fd0e60")
+        .permission(permission)
+        .build()
+    init {
+        trusteeClient.connectionCheck("/api/ids/description")
+        consumerClient.connectionCheck("/api/health")
+    }
+
+    fun registerAsset2(data: String): String{
+        // Provider registers asset with Trustee
+        val providerRequest = ProviderRequest(
+            "eurodat-connector-test",
+            "$providerIdsURL/api/v1/ids/data",
+            "owner-ID",
+            "persistent",
+            asset,
+            policy,
+            URI("urn:connector:provider"),
+            URI("urn:connector:consumer")
+        )
+        val providerRequestString = jsonMapper.writeValueAsString(providerRequest)
+
+        val assetResponse = trusteeClient.post("/api/asset/register", providerRequestString)
+        val assetId = assetResponse["asset"]["properties"]["asset:prop:id"].asText()
+        val contractDefinitionId = assetResponse["contractDefinition"]["id"].asText()
+
+        return assetId
     }
 }
