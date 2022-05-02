9172aee59266e208b4ae73cae0e4da644ccb5eea
Stephan Sinn <stephan.sinn@d-fine.de>
2022-05-02 13:37:18 +0200

    ktlintformat

--- dataland-consumer/build.gradle.kts
+++ dataland-consumer/build.gradle.kts
@@ -26,7 +26,6 @@ val sonarSources by extra(emptyList<File>())
 val jacocoSources by extra(emptyList<File>())
 val jacocoClasses by extra(emptyList<File>())
 
-
 plugins {
     `java-library`
     id("application")
