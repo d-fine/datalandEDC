99c14a0d8612b6b55307d0803d578c6bbef5f250
Emanuel Alexandi <emanuel.alexandi@d-fine.de>
2022-05-03 15:21:44 +0200

    small fix in settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,7 +2,6 @@ rootProject.name = "connector"
 includeBuild("trustee-platform/services/edc") // TODO
 includeBuild("trustee-platform/services/broker")
 include("api")
-include("dataland-connector")
 include("dataland-edc-client")
 include("dataland-edc-dummyserver")
 include("dataland-edc-server")
