e1f2c32b3669401f34f8393c0c0596b590d73d56
Emanuel Alexandi <emanuel.alexandi@d-fine.de>
2022-05-05 11:23:27 +0200

    small fix with "http" in curl tests

--- dataland-edc-server/prepare_tunnels_and_do_testcurls.sh
+++ dataland-edc-server/prepare_tunnels_and_do_testcurls.sh
@@ -46,7 +46,7 @@ sleep 20
 echo "Done waiting. Continue with test."
 
 echo "Checking health endpoint"
-health_response=$(curl -X GET "${dataland_edc_server_uri}:${dataland_edc_server_web_http_port}/api/health" -H "accept: application/json")
+health_response=$(curl -X GET "http://${dataland_edc_server_uri}:${dataland_edc_server_web_http_port}/api/health" -H "accept: application/json")
 if [[ ! $health_response =~ "I am alive!" ]]; then
   echo "Response was unexpected: $health_response"
   exit 1
@@ -57,11 +57,11 @@ test_data="Test Data from: "$(date "+%d.%m.%Y %H:%M:%S")
 start_time=$(date +%s)
 
 echo "Posting test data: $test_data."
-dataId=$(curl -X POST "${dataland_edc_server_uri}:${dataland_edc_server_web_http_port}/api/dataland/data" -H "accept: application/json" -H "Content-Type: application/json" -d "$test_data")
+dataId=$(curl -X POST "http://${dataland_edc_server_uri}:${dataland_edc_server_web_http_port}/api/dataland/data" -H "accept: application/json" -H "Content-Type: application/json" -d "$test_data")
 echo "Received response from post request: $dataId"
 
 echo "Retrieving test data."
-get_response=$(curl -X GET "${dataland_edc_server_uri}:${dataland_edc_server_web_http_port}/api/dataland/data/$dataId" -H "accept: application/json")
+get_response=$(curl -X GET "http://${dataland_edc_server_uri}:${dataland_edc_server_web_http_port}/api/dataland/data/$dataId" -H "accept: application/json")
 if [[ ! $get_response =~ $test_data ]]; then
   echo "Response was unexpected: $get_response"
   echo "Expected was substring: $test_data"
