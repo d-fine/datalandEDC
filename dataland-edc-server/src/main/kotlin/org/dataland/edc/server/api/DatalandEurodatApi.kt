b1f55d9ed665be39b8d4efd3c0448bf146dbf1bc
Emanuel Alexandi <emanuel.alexandi@d-fine.de>
2022-05-09 13:49:22 +0200

    Deleted copyright stuff and two obsolete properties in root gradle.properties

--- dataland-edc-server/src/main/kotlin/org/dataland/edc/server/api/DatalandEurodatApi.kt
+++ dataland-edc-server/src/main/kotlin/org/dataland/edc/server/api/DatalandEurodatApi.kt
@@ -1,16 +1,3 @@
-/*
- *  Copyright (c) 2021 Fraunhofer Institute for Software and Systems Engineering
- *
- *  This program and the accompanying materials are made available under the
- *  terms of the Apache License, Version 2.0 which is available at
- *  https://www.apache.org/licenses/LICENSE-2.0
- *
- *  SPDX-License-Identifier: Apache-2.0
- *
- *  Contributors:
- *       Fraunhofer Institute for Software and Systems Engineering - initial API and implementation
- *
- */
 package org.dataland.edc.server.api
 
 import jakarta.ws.rs.Consumes
