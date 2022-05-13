f89f8d754821fdbc353ae5ef58e004ef66391ec9
Emanuel Alexandi <emanuel.alexandi@d-fine.de>
2022-05-13 10:55:57 +0200

    More debugging

--- dataland-edc-server/test/e2etest.sh
+++ dataland-edc-server/test/e2etest.sh
@@ -5,6 +5,11 @@ workdir=$(dirname "$0")
 echo "Changing to working directory $workdir."
 cd "$workdir"
 
+echo "Present working directory:"
+pwd
+echo "Showing all contents in this folder"
+ls
+
 echo "A"
 envsubst < "./.env.e2etest" > .env
 echo "B"
