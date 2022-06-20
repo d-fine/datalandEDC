9d0ddad388a0c8abf6c9e2b8751019b44ae8c2d8
SiegfriedSobkowiak <71646830+SiegfriedSobkowiak@users.noreply.github.com>
2022-06-20 21:16:46 +0200

    update versions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,9 +16,9 @@ dependencyResolutionManagement {
             library("moshi-kotlin", "com.squareup.moshi:moshi-kotlin:1.13.0")
             library("moshi-adapters", "com.squareup.moshi:moshi-adapters:1.13.0")
 
-            library("swagger-jaxrs2-jakarta", "io.swagger.core.v3:swagger-jaxrs2-jakarta:2.2.0")
-            library("swagger-gradle-plugin", "io.swagger.core.v3:swagger-gradle-plugin:2.2.0")
-            library("swagger-annotations", "io.swagger.core.v3:swagger-annotations:2.2.0")
+            library("swagger-jaxrs2-jakarta", "io.swagger.core.v3:swagger-jaxrs2-jakarta:2.2.1")
+            library("swagger-gradle-plugin", "io.swagger.core.v3:swagger-gradle-plugin:2.2.1")
+            library("swagger-annotations", "io.swagger.core.v3:swagger-annotations:2.2.1")
 
             library("okhttp", "com.squareup.okhttp3:okhttp:4.9.3")
             library("rs-api", "jakarta.ws.rs:jakarta.ws.rs-api:3.1.0")
