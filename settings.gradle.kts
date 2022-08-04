8af4508bd04c5d9a6971705ce7b5782e20d32d6f
MarcTM01 <marc.meyer@d-fine.de>
2022-08-04 10:59:44 +0200

    Version updates

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,9 +8,9 @@ dependencyResolutionManagement {
         create("libs") {
             library("springdoc-openapi-ui", "org.springdoc:springdoc-openapi-ui:1.6.9")
 
-            library("junit-jupiter", "org.junit.jupiter:junit-jupiter:5.8.2")
-            library("junit-jupiter-engine", "org.junit.jupiter:junit-jupiter-engine:5.8.2")
-            library("junit-jupiter-api", "org.junit.jupiter:junit-jupiter-api:5.8.2")
+            library("junit-jupiter", "org.junit.jupiter:junit-jupiter:5.9.0")
+            library("junit-jupiter-engine", "org.junit.jupiter:junit-jupiter-engine:5.9.0")
+            library("junit-jupiter-api", "org.junit.jupiter:junit-jupiter-api:5.9.0")
 
             library("moshi-kotlin", "com.squareup.moshi:moshi-kotlin:1.13.0")
             library("moshi-adapters", "com.squareup.moshi:moshi-adapters:1.13.0")
@@ -19,8 +19,9 @@ dependencyResolutionManagement {
             library("swagger-gradle-plugin", "io.swagger.core.v3:swagger-gradle-plugin:2.2.1")
             library("swagger-annotations", "io.swagger.core.v3:swagger-annotations:2.2.1")
 
-            library("okhttp", "com.squareup.okhttp3:okhttp:4.9.3")
+            library("okhttp", "com.squareup.okhttp3:okhttp:4.10.0")
             library("rs-api", "jakarta.ws.rs:jakarta.ws.rs-api:3.1.0")
+            library("jakarta-annotation-api", "jakarta.annotation:jakarta.annotation-api:2.1.0")
 
             library("awaitility", "org.awaitility:awaitility:4.2.0")
             library("awaitility-kotlin", "org.awaitility:awaitility-kotlin:4.2.0")
