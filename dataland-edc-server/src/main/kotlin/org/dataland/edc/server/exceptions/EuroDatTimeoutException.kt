6e84bec1193fff973744641263b566d3bdfb2fc7
d92048 <stephan.waldow@d-fine.de>
2022-09-16 10:09:39 +0200

    removes not used code

--- dataland-edc-server/src/main/kotlin/org/dataland/edc/server/exceptions/EuroDatTimeoutException.kt
+++
@@ -1,6 +0,0 @@
-package org.dataland.edc.server.exceptions
-
-/**
- * Exception that can be thrown in case a Timeout with EuroDaT occurs
- */
-class EuroDatTimeoutException(message: String, cause: Throwable) : Exception(message, cause)
