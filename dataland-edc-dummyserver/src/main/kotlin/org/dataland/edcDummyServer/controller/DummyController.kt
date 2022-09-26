ba05dbf7dd679fc9ba1e1fa2a221ea6e74d14388
d92048 <stephan.waldow@d-fine.de>
2022-09-26 16:53:31 +0200

    tests modified dummy controller

--- dataland-edc-dummyserver/src/main/kotlin/org/dataland/edcDummyServer/controller/DummyController.kt
+++ dataland-edc-dummyserver/src/main/kotlin/org/dataland/edcDummyServer/controller/DummyController.kt
@@ -1,11 +1,16 @@
 package org.dataland.edcDummyServer.controller
 
+import io.swagger.v3.oas.annotations.Parameter
 import org.dataland.edcDummyServer.openApiServer.api.DefaultApi
 import org.dataland.edcDummyServer.openApiServer.model.CheckHealthResponse
 import org.dataland.edcDummyServer.openApiServer.model.InsertDataResponse
 import org.dataland.edcDummyServer.service.InMemoryDataStore
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import javax.validation.Valid
 
 /**
  * Implementation of API
@@ -14,11 +19,25 @@ import org.springframework.web.bind.annotation.RestController
 class DummyController : DefaultApi {
     val dataStore = InMemoryDataStore()
 
-    override fun selectDataById(dataId: String): ResponseEntity<String> {
+    override fun selectDataById(
+        @Parameter(
+            description = "",
+            required = true
+        ) @PathVariable(value = "dataId") dataId: String,
+        @Parameter(description = "") @Valid @RequestParam(
+            required = false,
+            value = "correlationId"
+        ) correlationId: String?
+    ): ResponseEntity<String> {
         return ResponseEntity.ok(dataStore.selectDataSet(dataId))
     }
 
-    override fun insertData(body: String?): ResponseEntity<InsertDataResponse> {
+    override fun insertData(
+        @Parameter(description = "") @Valid @RequestParam(
+            required = false,
+            value = "correlationId"
+        ) correlationId: String?, @Parameter(description = "") @Valid @RequestBody(required = false) body: String?
+    ): ResponseEntity<InsertDataResponse> {
         return ResponseEntity.ok(InsertDataResponse(dataStore.insertDataSet(body ?: "")))
     }
 
